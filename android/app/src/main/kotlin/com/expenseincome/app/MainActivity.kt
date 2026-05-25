9fe3ceffe7409e35dc5fcf239e077d62b8e31196
niravgohil <niravgohil58@gmail.com>
2026-05-25 16:47:42 +0530

    - version 1.0.0(4)

--- android/app/src/main/kotlin/com/expenseincome/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/expenseincome/app/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.expenseincome.app
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Enable edge-to-edge drawing programmatically using WindowCompat
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
